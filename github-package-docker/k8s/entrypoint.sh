fc625f97666c9ce91dc94b6e877d3f4e731e08b8
王佳 <wangjia_1919@163.com>
2019-12-31 17:00:44 +0800

    TEST
    Signed-off-by: 王佳 <wangjia_1919@163.com>

--- github-package-docker/k8s/entrypoint.sh
+++ github-package-docker/k8s/entrypoint.sh
@@ -5,24 +5,24 @@ KUBERNETES_RELEASE_VERSION="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
 KUBERNETES_VERSION=${INPUT_KUBERNETES_VERSION:=${KUBERNETES_RELEASE_VERSION}}
 echo "currect version ${KUBERNETES_VERSION}"
 
-curl -O -L https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubeadm
-chmod +x kubeadm
-images=$(./kubeadm config images list --kubernetes-version=${KUBERNETES_VERSION})
+# curl -O -L https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubeadm
+# chmod +x kubeadm
+# images=$(./kubeadm config images list --kubernetes-version=${KUBERNETES_VERSION})
 
-echo "image list: ${images}"
+# echo "image list: ${images}"
 
-echo "${INPUT_TOKEN}" | docker login --username ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
+# echo "${INPUT_TOKEN}" | docker login --username ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
 
-# docker tag IMAGE_ID docker.pkg.github.com/NineSwordsMonster/repository-name/IMAGE_NAME:VERSION
-while IFS='/' read key value; do
-    image=${key}/${value}
-    echo "base image >> ${image} <<"
-    docker pull ${image}
-    new_image=${INPUT_REPOSITORY}/${value}
-    if [ -n "${INPUT_REGISTRY}" ]; then
-        new_image=${INPUT_REGISTRY}/${new_image}
-    fi
-    echo "new image >> ${new_image} <<"
-    docker tag ${image} ${new_image}
-    docker push ${new_image}
-done <<< ${images}
+# # docker tag IMAGE_ID docker.pkg.github.com/NineSwordsMonster/repository-name/IMAGE_NAME:VERSION
+# while IFS='/' read key value; do
+#     image=${key}/${value}
+#     echo "base image >> ${image} <<"
+#     docker pull ${image}
+#     new_image=${INPUT_REPOSITORY}/${value}
+#     if [ -n "${INPUT_REGISTRY}" ]; then
+#         new_image=${INPUT_REGISTRY}/${new_image}
+#     fi
+#     echo "new image >> ${new_image} <<"
+#     docker tag ${image} ${new_image}
+#     docker push ${new_image}
+# done <<< ${images}
