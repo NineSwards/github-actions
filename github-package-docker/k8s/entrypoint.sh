2a0d3c261171a5036de4296c70d675e934ed90e2
王佳 <wangjia_1919@163.com>
2019-12-31 14:11:28 +0800

    rename

--- github-package-docker/k8s/entrypoint.sh
+++ github-package-docker/k8s/entrypoint.sh
@@ -14,10 +14,11 @@ echo "image list: ${images}"
 #echo "${INPUT_PASSWORD}"  | docker login ${INPUT_REGISTRY} --username ${INPUT_USERNAME} --password-stdin
 docker login -u ${INPUT_USERNAME} -p ${INPUT_TOKEN} ${INPUT_REGISTRY}
 
+# docker tag IMAGE_ID docker.pkg.github.com/NineSwordsMonster/repository-name/IMAGE_NAME:VERSION
 while IFS='/' read key value; do
     image=${key}/${value}
     docker pull ${image}
-    new_image=${INPUT_REPOSITORY}/${value}
+    new_image=${INPUT_USERNAME}/${INPUT_REPOSITORY}/${value}
     if [ -n "${INPUT_REGISTRY}" ]; then
         new_image=${INPUT_REGISTRY}/${new_image}
     fi
